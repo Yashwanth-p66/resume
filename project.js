1baeb54c561e2339b537098185b080340b95a057
Yashwanth-p66 <155279729+Yashwanth-p66@users.noreply.github.com>
2024-01-06 02:59:23 +0530

    Add files via upload

--- project.js
+++ project.js
@@ -1,8 +1,8 @@
-let menu=document.querySelector('#menu-icon');
-let navbar=document.querySelector('.navbar');
-menu.onclick=()=>{
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open')
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("open");
 };
 const sr = ScrollReveal({
   distance: "40px",
